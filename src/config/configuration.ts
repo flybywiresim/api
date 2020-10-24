f213c54520c6cc36e9da41cf7b22e33d8b8c538f
Niklas Steiner <niklas@sbg.at>
2020-10-25 00:28:42 +0200

    Enable CORS + Rate Limiting

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,5 +1,4 @@
 export default () => ({
-  port: parseInt(process.env.PORT) || 3000,
   database: {
     host: process.env.DATABASE_HOST || 'localhost',
     port: parseInt(process.env.DATABASE_PORT) || 3306,
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as requestIp from 'request-ip';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(requestIp.mw())
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(requestIp.mw());
+  app.set('trust proxy', 1);
+  app.use(helmet());
+  app.enableCors();
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
 
   await app.listen(3000);
 }
