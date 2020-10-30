e34e293417308b0c7896e1cda54a548c6d4c2f5a
Niklas Steiner <niklas@sbg.at>
2020-10-30 20:25:45 +0100

    Ensure a JWT secret is used in production

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
@@ -20,4 +20,13 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
   providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule {
+  private readonly logger = new Logger(AuthModule.name);
+
+  constructor() {
+    if (!process.env.AUTH_SECRET && process.env.NODE_ENV === 'production') {
+      this.logger.error('Use a JWT secret in production mode');
+      process.exit(99);
+    }
+  }
+}
