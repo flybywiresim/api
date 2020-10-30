cf4eefb88b92ea6c1c3687f12230a966a96dbae7
Niklas Steiner <niklas@sbg.at>
2020-10-30 22:23:12 +0100

    Move decorator

---
+++ src/utilities/ip-address.decorator.ts
@@ -0,0 +1,11 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+import * as requestIp from 'request-ip';
+
+export const IpAddress = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest();
+
+  if (request.clientIp)
+    return request.clientIp;
+  return requestIp.getClientIp(request); // In case we forgot to include requestIp.mw() in main.ts
+});
