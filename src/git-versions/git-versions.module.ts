fe678e1dc88debd7f16cb2ff144650a129434a7b
Niklas Steiner <niklas@sbg.at>
2020-12-28 02:49:01 +0100

    Add GitHub versions info

---
+++ src/git-versions/git-versions.module.ts
@@ -0,0 +1,10 @@
+import { HttpModule, Module } from '@nestjs/common';
+import { GitVersionsService } from './git-versions.service';
+import { GitVersionsController } from './git-versions.controller';
+
+@Module({
+  imports: [HttpModule],
+  providers: [GitVersionsService],
+  controllers: [GitVersionsController]
+})
+export class GitVersionsModule {}
---
+++ src/git-versions/git-versions.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { GitVersionsService } from './git-versions.service';
+
+describe('GitVersionsService', () => {
+  let service: GitVersionsService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [GitVersionsService],
+    }).compile();
+
+    service = module.get<GitVersionsService>(GitVersionsService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
