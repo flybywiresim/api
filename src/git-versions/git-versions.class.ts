09f32971219f442474020908d67482e7db864ad5
Niklas Steiner <niklas@sbg.at>
2021-01-26 01:52:27 +0100

    feat: add pull info

--- src/git-versions/git-versions.class.ts
+++ src/git-versions/git-versions.class.ts
@@ -18,3 +18,20 @@ export class ReleaseInfo {
   @ApiProperty({ description: 'The URL of the release page on GitHub' })
   htmlUrl: string;
 }
+
+export class PullInfo {
+  @ApiProperty({ description: 'The number of the PR', example: '3176' })
+  number: number;
+
+  @ApiProperty({ description: 'The title of the PR', example: 'feat: brake fan' })
+  title: string;
+
+  @ApiProperty({ description: 'The username of the author', example: 'tyler58546' })
+  author: string;
+
+  @ApiProperty({ description: 'The labels on the PR', example: ['Ready to Test', 'QA Tier 2']})
+  labels: string[];
+
+  @ApiProperty({ description: 'Whether the PR is still a draft' })
+  isDraft: boolean;
+}
