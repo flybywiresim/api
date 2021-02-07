e31030790eb7cca1480e095aa4a4c60dd9508914
pepperoni505 <49950581+pepperoni505@users.noreply.github.com>
2021-02-06 23:06:04 -0500

    fix: update ttl to an hour

--- src/charts/charts.controller.ts
+++ src/charts/charts.controller.ts
@@ -12,7 +12,7 @@ export class ChartsController {
   }
 
   @Get(':icao')
-  @CacheTTL(120)
+  @CacheTTL(3600)
   @ApiParam({ name: 'icao', description: 'The ICAO of the airport to search for', example: 'KLAX' })
   @ApiOkResponse({ description: 'Charts were found', type: Charts })
   @ApiNotFoundResponse({ description: 'Charts not available for ICAO' })
