c1b8b40431ce07fc7f061f8aac1732cf46d9d5ce
Ludovic Lefortt <ludovic@consultpoint.net>
2021-05-23 21:21:30 +0200

    implement online controller for vatsim atc

---
+++ src/online/atc-info.class.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ATCInfo {
+  @ApiProperty({ description: 'The atc callsign', example: 'EBBR_TWR' })
+  callsign:string;
+
+  @ApiProperty({ description: 'The atc frequency', example: '128.800' })
+  frequency:string;
+
+  @ApiProperty({ description: 'The atc visual range', example: 150 })
+  visualRange:number;
+
+  @ApiProperty({ description: 'The atc current ATIS', example: ['line 1', 'line2', 'line3'] })
+  textAtis: string[];
+
+  @ApiProperty({ description: 'The atc type', example: 'GND' })
+  type:string;
+
+  @ApiProperty({ description: 'The atc latitude', example: 32.08420727935125 })
+  latitude?: number;
+
+  @ApiProperty({ description: 'The atc longitude', example: -81.14929543157402 })
+  longitude?: number;
+}
---
+++ src/online/online.controller.ts
@@ -0,0 +1,43 @@
+import {
+    CacheInterceptor,
+    CacheTTL,
+    Controller,
+    Get,
+    Query,
+    UseInterceptors,
+} from '@nestjs/common';
+import {
+    ApiOkResponse,
+    ApiQuery,
+    ApiTags,
+} from '@nestjs/swagger';
+import { exception } from 'console';
+import { ATCInfo } from './atc-info.class';
+import { VatsimService } from './vatsim.service';
+
+@ApiTags('ONLINE')
+@Controller('online')
+@UseInterceptors(CacheInterceptor)
+export class OnlineController {
+    constructor(private vatsimService: VatsimService) {}
+
+    @Get('atc')
+    @CacheTTL(120)
+    @ApiQuery({
+        name: 'source',
+        description: 'The source for the atcs',
+        example: 'vatsim',
+        required: true,
+        enum: ['vatsim', 'ivao'],
+    })
+    @ApiOkResponse({ description: 'list of connected atc', type: ATCInfo })
+    async getControllers(@Query('source') source?: string): Promise<ATCInfo[]> {
+        if (source === 'vatsim') {
+            return this.vatsimService.getControllers();
+        }
+        if (source === 'ivao') {
+            return [];
+        }
+        return null;
+    }
+}
