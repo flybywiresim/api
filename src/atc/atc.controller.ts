be07ab0053e856136f8eb5462acc185a4ca66746
Ludovic Lefortt <ludovic@consultpoint.net>
2021-06-15 08:54:40 +0200

    version on api

--- src/atc/atc.controller.ts
+++ src/atc/atc.controller.ts
@@ -15,7 +15,7 @@ import { ATCInfo } from './atc-info.class';
 import { AtcService } from './atc.service';
 
 @ApiTags('ATC')
-@Controller('atc')
+@Controller('v1/atc')
 @UseInterceptors(CacheInterceptor)
 export class AtcController {
     constructor(private atcService: AtcService) {}
